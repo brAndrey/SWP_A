546e89f1177548fc8b22af87cf46d55856dacd0f
andrey_vb <train1284@mail.ru>
2020-07-20 14:54:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/gdgkazan/simpleweather/screen/weatherlist/CityHolder.java
@@ -0,0 +1,39 @@
+package ru.gdgkazan.simpleweather.screen.weatherlist;
+
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.gdgkazan.simpleweather.R;
+import ru.gdgkazan.simpleweather.data.model.City;
+
+/**
+ * @author Artur Vasilov
+ */
+public class CityHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.city_name)
+    TextView mCityName;
+
+    @BindView(R.id.temperature)
+    TextView mTemperature;
+
+    public CityHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(@NonNull City city) {
+        if (mCityName != null) {
+            mCityName.setText(city.getName());
+            if (city.getMain() != null) {
+                String temp = mTemperature.getResources().getString(R.string.f_temperature, city.getMain().getTemp());
+                mTemperature.setText(temp);
+            }
+        }
+    }
+}
